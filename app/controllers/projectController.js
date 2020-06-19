e19153ccc82035ab5749d5fda7268f926f3f51d4
JuniorAlbuquerque <junioralbuquerque81@gmail.com>
2020-06-18 20:58:33 -0400

    chore: add pdf download in route

--- app/controllers/projectController.js
+++ app/controllers/projectController.js
@@ -1,4 +1,8 @@
 const knex = require('../database')
+const pdf = require('html-pdf')
+const ejs = require('ejs')
+const path = require('path')
+const { getDate, formatDate } = require('../utils/date')
 
 exports.getProjectById = async (req, res) => {
   const { id_project } = req.params
@@ -58,6 +62,78 @@ exports.createProject = async (req, res, next) => {
     return res.status(401).json({
       message: 'Não autorizado',
     })
+  } catch (error) {
+    next(error)
+  }
+}
+
+exports.getPdf = async (req, res, next) => {
+  const { id_project } = req.params
+
+  try {
+    const Project = await knex('project')
+      .join('administrator', 'project.id_admin', '=', 'administrator.id_admin')
+      .where('id_project', id_project)
+      .select(
+        knex.ref('project.nome').as('Projeto'),
+        'project.descricao',
+        'project.created_at',
+      )
+      .select(knex.ref('administrator.nome').as('Responsável'))
+      .first()
+    const subs = []
+
+    const creationDate = formatDate(
+      Project.created_at.toLocaleDateString(),
+      'pt-br',
+    )
+
+    await knex('subsystem')
+      .where('id_project', id_project)
+      .then((sub) => {
+        sub.map(async (sb) => {
+          const reqFunc = await knex('reqfunctional').where('id_sub', sb.id_sub)
+          const reqNonFunc = await knex('reqnonfunctional').where(
+            'id_sub',
+            sb.id_sub,
+          )
+          const nome = sb.nome
+          const descricaoSub = sb.descricao
+          subs.push({ nome, descricaoSub, reqFunc, reqNonFunc })
+        })
+
+        setTimeout(() => {
+          ejs.renderFile(
+            './template.ejs',
+            {
+              Project,
+              DataAtual: getDate(),
+              creationDate,
+              Subsystems: subs,
+            },
+            (err, html) => {
+              if (err) {
+                res.json({ message: 'Erro ejs' })
+              } else {
+                pdf
+                  .create(html, {})
+                  .toFile(
+                    `./app/temp/projeto_${id_project}.pdf`,
+                    (err, resp) => {
+                      if (err) {
+                        res.json({ message: 'erro pdf' })
+                      } else {
+                        res.sendFile(
+                          path.resolve(`app/temp/projeto_${id_project}.pdf`),
+                        )
+                      }
+                    },
+                  )
+              }
+            },
+          )
+        }, 200)
+      })
   } catch (error) {
     next(error)
   }
--- app/routes/projectRouter.js
+++ app/routes/projectRouter.js
@@ -6,5 +6,6 @@ const isLogged = require('../middleware/login')
 projectRouter.get('/:id_admin', isLogged, projectController.getProjectByAdmin)
 projectRouter.get('/info/:id_project', projectController.getProjectById)
 projectRouter.post('/create', isLogged, projectController.createProject)
+projectRouter.get('/downloadpdf/:id_project', projectController.getPdf)
 
 module.exports = projectRouter
