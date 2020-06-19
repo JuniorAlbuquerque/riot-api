9e6f241cc83d9c94d2a21a237da6e6d397eaa41c
JuniorAlbuquerque <junioralbuquerque81@gmail.com>
2020-06-19 00:38:23 -0400

    chore: add auto increment indicator requirement

--- app/controllers/requirementController.js
+++ app/controllers/requirementController.js
@@ -1,12 +1,29 @@
 const knex = require('../database')
 
+const last = (requirement) => requirement[requirement.length - 1]
+const intReq = (num) => parseInt(num.replace(/^\D+/g, ''))
+let RF
+let RNF
+
 exports.createRequirement = async (req, res, next) => {
   try {
-    const { id_sub, indicador, descricao, tipo } = req.body
+    const { id_sub, descricao, tipo } = req.body
 
     if (tipo) {
+      await knex('reqnonfunctional')
+        .where('id_sub', id_sub)
+        .then(async (reqNonFunc) => {
+          if (reqNonFunc.length > 0) {
+            const lastRNFIndicator = last(reqNonFunc).indicador
+            const num = intReq(lastRNFIndicator)
+            RNF = num < 9 ? 'RNF0' + (num + 1) : 'RNF' + (num + 1)
+          } else {
+            RNF = 'RNF01'
+          }
+        })
+
       await knex('reqnonfunctional').insert({
-        indicador,
+        indicador: RNF,
         id_sub,
         tipo,
         descricao,
@@ -17,8 +34,20 @@ exports.createRequirement = async (req, res, next) => {
       })
     }
 
+    await knex('reqfunctional')
+      .where('id_sub', id_sub)
+      .then(async (reqFunc) => {
+        if (reqFunc.length > 0) {
+          const lastRFIndicator = last(reqFunc).indicador
+          const num = intReq(lastRFIndicator)
+          RF = num < 9 ? 'RF0' + (num + 1) : 'RF' + (num + 1)
+        } else {
+          RF = 'RF01'
+        }
+      })
+
     await knex('reqfunctional').insert({
-      indicador,
+      indicador: RF,
       id_sub,
       descricao,
     })
