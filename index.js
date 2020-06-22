7d086997240aae92d572e952d6b2048fbe25b926
JuniorAlbuquerque <junioralbuquerque81@gmail.com>
2020-06-22 14:18:57 -0400

    chore: deployb config

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const routes = require('./app/routes')
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
