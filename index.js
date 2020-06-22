ef5a7cc84c73ec85c4b3480603fef3c088c56f90
JuniorAlbuquerque <junioralbuquerque81@gmail.com>
2020-06-22 14:44:16 -0400

    fix: add cors

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const routes = require('./app/routes')
-// const cors = require('cors')
+const cors = require('cors')
 
-// app.use(cors())
+app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
