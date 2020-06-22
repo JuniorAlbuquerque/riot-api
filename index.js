d057ee6ba3ca1415dbd0ce948d38660dfe93413e
JuniorAlbuquerque <junioralbuquerque81@gmail.com>
2020-06-22 14:30:01 -0400

    fix: routes

--- index.js
+++ index.js
@@ -3,39 +3,39 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const routes = require('./app/routes')
-const cors = require('cors')
+// const cors = require('cors')
 
-app.use(cors())
-app.use(express.json())
+// app.use(cors())
 app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*')
-//   res.header(
-//     'Access-Control-Allow-Headers',
-//     'Origin, X-Requeted-With, Content-Type, Accept, Authorization, RBR',
-//   )
-//   if (req.headers.origin) {
-//     res.header('Access-Control-Allow-Origin', req.headers.origin)
-//   }
-//   if (req.method === 'OPTIONS') {
-//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
-//     return res.status(200).json({})
-//   }
-//   next()
-// })
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requeted-With, Content-Type, Accept, Authorization, RBR',
+  )
+  if (req.headers.origin) {
+    res.header('Access-Control-Allow-Origin', req.headers.origin)
+  }
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
+    return res.status(200).json({})
+  }
+  next()
+})
 
 app.use(routes)
 
-// app.use((req, res, next) => {
-//   const error = new Error('Not found')
-//   error.status = 404
-//   next(error)
-// })
+app.use((req, res, next) => {
+  const error = new Error('Not found')
+  error.status = 404
+  next(error)
+})
 
-// app.use((error, req, res, next) => {
-//   res.status(error.status || 500)
-//   res.json({ error: error.message })
-// })
+app.use((error, req, res, next) => {
+  res.status(error.status || 500)
+  res.json({ error: error.message })
+})
 
-app.listen(process.env.PORT || 3020)
+app.listen(process.env.PORT || 3000)
